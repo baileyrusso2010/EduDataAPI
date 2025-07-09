00ea5d4a42f70e8cbfcb82e0de7a9fb451cc95b0
baileyrusso2010 <baileyrusso3371@gmail.com>
2025-07-08 22:55:11 -0400

    Minor updates

--- src/models/mtss/student_tier.model.ts
+++ src/models/mtss/student_tier.model.ts
@@ -1,48 +1,45 @@
-import { Model, DataTypes } from "sequelize"
-import sequelize from "../../database"
-import { Tier } from "./tier.model"
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../../database";
+import { Tier } from "./tier.model";
 
 export class StudentTier extends Model {
-    declare id: number
-    declare studentId: number
-    declare tierId: number
-    declare assigned_date: Date
-    declare end_date: Date | null
-    declare notes: string | null
+  declare id: number;
+  declare studentId: number;
+  declare tierId: number;
+  declare assigned_date: Date;
+  declare end_date: Date | null;
+  declare notes: string | null;
 
-    // Add this line:
-    declare Tier?: Tier
+  // Add this line:
+  declare Tier?: Tier;
 }
 
 StudentTier.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        studentId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        tierId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        assigned_date: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        end_date: {
-            type: DataTypes.DATE,
-        },
-        notes: {
-            type: DataTypes.TEXT,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        sequelize,
-        tableName: "student_tier",
-        timestamps: false,
-    }
-)
+    studentId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    tierId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    assigned_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    end_date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    tableName: "student_tier",
+    timestamps: false,
+  }
+);
